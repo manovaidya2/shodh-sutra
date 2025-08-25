238fffd92f30c8279fb7177c6ad490a84fd2881a
aviraj0403 <aviraj0403@gmail.com>
2025-08-26 00:33:39 +0530

    add port

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4100,  // Set the port to 4100
+    host: '0.0.0.0',  // Make the app accessible from external IPs
+  },
 })
