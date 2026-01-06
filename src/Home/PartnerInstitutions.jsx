a6f9be63a0de79edf47094195b90e2ae08fb8dba
Abhishek Kumar <abhishek8579013@gmail.com>
2026-01-06 15:18:43 +0530

    updae

--- src/Home/PartnerInstitutions.jsx
+++ src/Home/PartnerInstitutions.jsx
@@ -2,32 +2,56 @@ import React from 'react';
 import '../styles/PartnerInstitutions.css';
 
 // Import local images
-import harvardImg from '../images/Rectangle 9.jpg';
-import stanfordImg from '../images/Rectangle 11.jpg';
-import princetonImg from '../images/Rectangle 10.jpg';
+import associateImg1 from '../about/image/about-img.jpg';
+import associateImg2 from '../about/image/5e8ed2bbe4124_corona_banner.jpg';
+
+import inspirationImg1 from '../images/delhi.jpeg';
+import inspirationImg2 from '../images/jnu.jpeg';
+import inspirationImg3 from '../images/ipu.jpeg';
 
 const PartnerInstitutions = () => {
   return (
     <div className="container">
       <div className="content">
-        <h1 className="title">Partner Institutions</h1>
+
+        {/* OUR ASSOCIATES */}
+        <h1 className="title">Our Associates</h1>
         <p className="description1">
-         ShodhSutra collaborates with UGC-recognized Indian universities and reputed international institutions to provide scholars with credible, compliant, and globally aligned research opportunities.
+          ShodhSutra collaborates with UGC-recognized Indian universities and reputed international institutions to provide scholars with credible, compliant, and globally aligned research opportunities.
         </p>
+
         <div className="institutions">
           <div className="institution-card">
-            <img src={harvardImg} alt="Harvard University" className="institution-image" />
-            <h3 className="institution-name">Harvard University</h3>
+            <img src={associateImg1} alt="Associate 1" className="institution-image" />
+            <h3 className="institution-name">North East Christian University (NECU)</h3>
           </div>
+
           <div className="institution-card">
-            <img src={stanfordImg} alt="Stanford University" className="institution-image" />
-            <h3 className="institution-name">Stanford University</h3>
-          </div>
-          <div className="institution-card">
-            <img src={princetonImg} alt="Princeton University" className="institution-image" />
-            <h3 className="institution-name">Princeton University</h3>
+            <img src={associateImg2} alt="Associate 2" className="institution-image" />
+            <h3 className="institution-name">Maharaja Agrasen Himalayan Garhwal University (MAHGU)</h3>
           </div>
         </div>
+
+        {/* OUR INSPIRATION */}
+        <h1 className="title inspiration-title">Our Inspiration</h1>
+
+        <div className="institutions">
+          <div className="institution-card">
+            <img src={inspirationImg1} alt="Inspiration 1" className="institution-image" />
+            <h3 className="institution-name">University of Delhi (DU)</h3>
+          </div>
+
+          <div className="institution-card">
+            <img src={inspirationImg2} alt="Inspiration 2" className="institution-image" />
+            <h3 className="institution-name">Jawaharlal Nehru University (JNU)</h3>
+          </div>
+
+          <div className="institution-card">
+            <img src={inspirationImg3} alt="Inspiration 3" className="institution-image" />
+            <h3 className="institution-name">Indraprastha University (IPU)</h3>
+          </div>
+        </div>
+
       </div>
     </div>
   );
